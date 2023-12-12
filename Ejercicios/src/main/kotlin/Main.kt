61b9628cac34b99ee70e12bd6e76a8d363873385
lnarote777 <nolara@iesrafaelalberti.es>
2023-12-12 20:14:54 +0100

    ...

---
+++ Ejercicios/src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main(){
+    /*TODO: Escribe un programa que le pida al usuario una temperatura en grados Celsius, la convierta a grados
+    Fahrenheit e imprima por pantalla la temperatura convertida.
+     */
+   // ej_1_4()
+
+    /*TODO: Escribe un programa que pida el importe final de un artículo y calcule e imprima por pantalla el IVA
+    que se ha pagado y el importe sin IVA (suponiendo que se ha aplicado un tipo de IVA del 10%).
+     */
+   // ej_1_6()
+
+    /*TODO:Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros),
+    calcule el índice de masa corporal y lo almacene en una variable, y muestre por pantalla la frase
+    Tu índice de masa corporal es donde es el índice de masa corporal calculado redondeado con dos decimales.
+     */
+   // ej_1_12()
+
+    /*
+    TODO: Imagina que acabas de abrir una nueva cuenta de ahorros que te ofrece el 4% de interés al año.
+    Estos ahorros debido a intereses, que no se cobran hasta finales de año, se te añaden al balance final de tu cuenta de ahorros.
+    Escribir un programa que comience leyendo la cantidad de dinero depositada en la cuenta de ahorros, introducida por el usuario.
+    Después el programa debe calcular y mostrar por pantalla la cantidad de ahorros tras el primer, segundo y tercer años. Redondear cada cantidad a dos decimales.
+     */
+    ej_1_15()
+}
